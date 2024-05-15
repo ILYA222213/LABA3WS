092e249de344c3665fce82f7fabd0d4b7ecebdce
Сом <117897751+ILYA222213@users.noreply.github.com>
2024-05-15 08:10:25 +0300

    Update Program.cs

--- LABA3WS/laba 3/Program.cs
+++ LABA3WS/laba 3/Program.cs
@@ -2,25 +2,35 @@
 
 struct Student
 {
+   // Поля структуры: полное имя, номер группы, массив с оценками
     public string FullName;
     public string GroupNumber;
     public int[] Performance;
 
+    // Метод, который вычисляет средний балл студента
     public double GetAveragePerformance()
     {
+        // Инициализируем переменную для хранения суммы оценок
         double sum = 0;
+
+        // Проходим по массиву оценок и суммируем их
         foreach (int mark in Performance)
         {
             sum += mark;
         }
+
+        // Возвращаем средний балл, который равен сумме оценок, деленной на количество оценок
         return sum / Performance.Length;
     }
 }
 
+// Основной класс программы
 class Program
 {
+    // Точка входа в программу
     static void Main(string[] args)
     {
+        // Создаем массив из 7 студентов с заданными параметрами
         Student[] students = new Student[7]
         {
             new Student { FullName = "Гурдин Р.Н", GroupNumber = "Группа 22ИТ35", Performance = new int[]{4, 5, 3, 5, 4} },
@@ -32,8 +42,10 @@ class Program
             new Student { FullName = "Новиков А.А", GroupNumber = "Группа 23ИТ12", Performance = new int[]{3, 3, 2, 4, 3} }
         };
 
+        // Сортируем массив студентов по возрастанию среднего балла
         Array.Sort(students, (x, y) => x.GetAveragePerformance().CompareTo(y.GetAveragePerformance()));
 
+        // Выводим информацию о каждом студенте
         foreach (var student in students)
         {
             Console.WriteLine($"Студент: {student.FullName}, Группа: {student.GroupNumber}, Средний балл: {student.GetAveragePerformance()}");
